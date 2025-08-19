91bed1b10bc6daae2c4e44f501b60195bbcea5df
Amadeo Abqary Dhiyaulhaq <deo531.id@gmail.com>
2025-08-19 09:47:34 +0700

    Update AccountTest.java

--- AccountTest.java
+++ AccountTest.java
@@ -2,14 +2,15 @@ public class AccountTest {
     //Data Pengguna Bank
     public static void main(String[] args) {
         SavingsAccount account1 = new SavingsAccount();
-        account1.nama = "John Doe";
-        account1.saldo = 1000.0;
+        account1.nama = "Ilham";
+        account1.saldo = 10000.0;
         account1.bunga = 0.02;
         account1.cetak();
 
         SavingsAccount account2 = new SavingsAccount();
-        account2.nama = "Jane Smith";
-        account2.saldo = 2000.0;
+        account2.nama = "AUrel";
+        account2.saldo = 20000.0;
         account2.cetak();
     }
 }
+
