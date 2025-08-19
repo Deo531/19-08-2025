2fd022b248239841d6709796cab8e04a001dfc2b
Amadeo Abqary Dhiyaulhaq <deo531.id@gmail.com>
2025-08-19 09:47:58 +0700

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -3,10 +3,11 @@ public class SavingsAccount {
     double bunga = 0.02;
     String nama;
     public void cetak(){
-        System.out.println("customer name: " + nama);
+        System.out.println("Nama Customer: " + nama);
         System.out.println("balance: " + saldo);
         System.out.println("interest rate: " + bunga);
         System.out.println("interest earned: " + (saldo * bunga));
         System.out.println();
     }
 }
+
